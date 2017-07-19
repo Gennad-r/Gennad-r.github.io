465b0c1e1a0716d01515d4220164e8ec6ecc054e
Gennad-r <gennad.r@gmail.com>
2017-07-19 09:47:59 +0300

    no russian, app lincs style changed, app.html added

--- js/script.js
+++ js/script.js
@@ -103,7 +103,7 @@ function initMap() {
 	var marker = new google.maps.Marker({
 		map: map,
 		position: dakarTaxi,
-		icon: './img/head-car-map.png',
+		icon: '/img/head-car-map.png',
 		title: 'Dakar Taxi office'
 	});
 
@@ -164,3 +164,40 @@ function action (e) {
 }
 })();
 //----------------Ajax query----------------------
+
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "/mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			$('form').fadeOut('slow', function () { $('.panel .panel-body').css({'opacity': '1'}); } );
+		});
+		return false;
+	});
+
+
+
+
+// Google Analitics
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-49350407-3', 'auto');
+  ga('send', 'pageview');
+
+
+
+
+
+
+
+//IP
+/*var ipData;
+$.getJSON('//freegeoip.net/json/?callback=?', function(data) {
+  ipData = data;
+});
+*/
